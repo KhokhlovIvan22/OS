663a81fc8a47e66080d3416391a2516a78661f0b
Ivan Khokhlov <khokhlovivan22@gmail.com>
2025-09-20 18:28:54 +0300

    Update creator.cpp

--- lab1/Creator/creator.cpp
+++ lab1/Creator/creator.cpp
@@ -2,45 +2,60 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <algorithm>
-#include <iomanip>
-#include <stdexcept>
-using std::string;
-using std::stod;
+#include <exception>
+using std::stoi;
 using std::cerr;
 using std::out_of_range;
 using std::exception;
-using std::sort;
 using std::runtime_error;
+using std::ios;
+using std::string;
+using std::ofstream;
 using std::cout;
+using std::cin;
 
 int main(int argc, char* argv[]) {
-    if (argc != 4) {
+    if (argc != 3) {
         cerr << "Error: invalid startup format\n";
         return 1;
     }
 
     string binFileName = argv[1];
-    string reportFileName = argv[2];
-    double rate = 0.0;
+    int recordCount = 0;
     try {
-        rate = stod(argv[3]);
-        if (rate <= 0.0)
-            throw out_of_range("Error: hourly rate must be greater than zero");
+        recordCount = stoi(argv[2]);
+        if (recordCount <= 0)
+            throw out_of_range("Error: amount of records must be greater than zero");
     }
     catch (const exception& ex) {
         cerr << ex.what() << "\n";
         return 2;
     }
 
-    vector <employee> employees;
-    if (readFromBinary(binFileName, employees) != 0)
+    try {
+        ofstream outFile(binFileName, ios::binary | ios::trunc);
+        if (!outFile.is_open()) {
+            throw runtime_error("Error: сould not open file for writing");
+        }
+        employee e;
+        string tmpName;
+        for (int i = 0; i < recordCount; ++i) {
+            cout << "\n\n";
+            cout <<"Record No. " << (i + 1) << "\n";
+            cin >> e;
+            if (!cin)
+                throw runtime_error("Input error");
+            outFile.write(reinterpret_cast<const char*>(&e), sizeof(e));
+            if (!outFile)
+                throw runtime_error("Write error");
+        }
+        outFile.close();
+    }
+    catch (const exception& ex) {
+        cerr << ex.what() << "\n";
         return 3;
-    sort(employees.begin(), employees.end(), [](const employee& a, const employee& b) {return a.num < b.num;});
-    if (writeReport(reportFileName, binFileName, employees, rate) != 0)
-        return 3;
+    }
 
-    cout << "Report successfully written to file \"" << reportFileName << "\"\n";
+    cout << "Data successfully written to file \"" << binFileName << " \"\n";
     return 0;
 }
