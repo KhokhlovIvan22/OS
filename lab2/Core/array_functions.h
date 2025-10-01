29fbcca3f705822f9db6e2140b179678674781b1
Ivan Khokhlov <khokhlovivan22@gmail.com>
2025-10-02 00:25:20 +0300

    Create array_functions.h

---
+++ lab2/Core/array_functions.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using std::istream;
+using std::ostream;
+using std::vector;
+
+istream& operator >> (istream& in, vector<int>& arr);
+ostream& operator << (ostream& out, const vector<int>& arr);
+void modify(vector<int>& arr, int min, int max, double avg);
