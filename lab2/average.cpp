d9644eb058cee6afca86cfb93edf4eae667c098a
Ivan Khokhlov <khokhlovivan22@gmail.com>
2025-10-02 19:39:33 +0300

    average element counting logic changed

--- lab2/average.cpp
+++ lab2/average.cpp
@@ -3,20 +3,20 @@
 #include <vector>
 using std::vector;
 using std::cout;
-const int sleepTime = 12;
 
 DWORD WINAPI averageThread(LPVOID param) {
 	AverageEl* args = static_cast<AverageEl*>(param);
 	vector<int>& a = args->arr;
 	if (a.empty())
 		return 0;
-	long long sum = 0.0;
+	long long sum = 0;
 	for (int i = 0; i < a.size(); ++i)
 	{
 		sum += a[i];
-		Sleep(sleepTime);
+		Sleep(averageSleepTime);
 	}
-	args->avg = sum / a.size();
+	double acc = static_cast<double>(sum) / a.size();
+	args->avg = static_cast<int>(acc >= 0 ? acc + 0.5 : acc - 0.5);
 	cout << "Average element = " << args->avg <<  "\n";
 	return 0;
 }
