4b770b9c8ca38a71fac6458b701c43f6706ad7c5
Ivan Khokhlov <khokhlovivan22@gmail.com>
2025-10-25 14:56:16 +0300

    Add files via upload

---
+++ kollok/tasks.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include <list.h>
+using std::vector;
+
+vector<unsigned long long> fibonacci(int n);
+bool isPalindrome(long long x);
+List reverseList(const List& orig);
