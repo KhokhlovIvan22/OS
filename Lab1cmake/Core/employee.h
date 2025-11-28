fd7ba8ec0a14eba627a80cc97d312d22195247fa
Ivan Khokhlov <khokhlovivan22@gmail.com>
2025-11-28 10:15:38 +0300

    Add files via upload

---
+++ Lab1cmake/Core/employee.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <fstream>
+#include <vector>
+#include <string>
+using std::istream;
+using std::ofstream;
+using std::vector;
+using std::string;
+using std::pair;
+
+struct employee
+{
+	int num;
+	char name[10];
+	double hours;
+	friend istream& operator >> (istream& in, employee& emp);
+	friend ofstream& operator << (ofstream& out, const pair <employee, double>& emp);
+	bool operator < (const employee& other) const;
+};
+
+int readFromBinary (const string& binFileName, vector <employee>& employees);
+int writeReport(const string& outFileName, const string& binFileName, const vector <employee>& employees, double rate);
